8c555ee6372d1c00852d38b18d33a6f5d3a4f600
Daniel Pope <mauve@mauveweb.co.uk>
2019-09-22 10:34:19 +0100

    Add joystick support

--- ascent.py
+++ ascent.py
@@ -1,10 +1,15 @@
+from dataclasses import dataclass
 import numpy as np
 import math
-from wasabi2d import Scene, event, run, Vector2
+from pygame import joystick
+from wasabi2d import Scene, event, run, Vector2, keyboard
 
 
 scene = Scene(title="Ascent - PyWeek 28")
 scene.layers[0].set_effect('bloom', radius=10)
+
+joystick.init()
+
 
 
 class Knight:
@@ -75,18 +80,49 @@ class Knight:
 knight = Knight()
 
 
+@dataclass
+class JoyController:
+    pc: Knight
+    stick: joystick.Joystick
+
+    def __post_init__(self):
+        self.stick.init()
+
+    def update(self):
+        self.pc.accelerate((
+            self.stick.get_axis(0),
+            self.stick.get_axis(1),
+        ))
+
+
+@dataclass
+class KeyboardController:
+    pc: Knight
+
+    def update(self):
+        ax = ay = 0
+        if keyboard.left:
+            ax = -1
+        elif keyboard.right:
+            ax = 1
+
+        if keyboard.up:
+            ay = -1
+        elif keyboard.down:
+            ay = 1
+
+        knight.accelerate((ax, ay))
+
+
+if joystick.get_count() > 0:
+    controller = JoyController(knight, joystick.Joystick(0))
+else:
+    controller = KeyboardController(knight)
+
+
 @event
 def update(dt, keyboard):
-    if keyboard.left:
-        knight.accelerate((-1, 0))
-    elif keyboard.right:
-        knight.accelerate((1, 0))
-
-    if keyboard.up:
-        knight.accelerate((0, -1))
-    elif keyboard.down:
-        knight.accelerate((0, 1))
-
+    controller.update()
     knight.update(dt)
 
 
