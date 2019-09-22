ba8249294eabb684cb8e1bbd28bd3a62300c7fe1
Larry Hastings <larry@hastings.org>
2019-09-22 11:27:32 -0700

    Added some enemies and shots.

--- proto.py
+++ proto.py
@@ -1,5 +1,6 @@
 import sys
 import math
+import random
 import wasabi2d
 from wasabi2d import Scene, run, event, clock, Vector2, keys
 import pygame.mouse
@@ -10,11 +11,21 @@ scene = Scene(1024, 768)
 
 # The rest of your code goes here.
 
-circle = scene.layers[0].add_circle(
-    radius=10,
-    pos=(scene.width / 2, scene.height / 2),
-    color=(0, 128, 0),
-)
+screen_center = Vector2(scene.width / 2, scene.height / 2)
+
+class Player:
+    def __init__(self):
+        self.pos = screen_center
+        self.shape = scene.layers[0].add_circle(
+            radius=10,
+            pos=self.pos,
+            color=(0, 128, 0),
+        )
+
+player = Player()
+
+def player_collided(other):
+    print("Player collided with", other)
 
 sword_and_shield = scene.layers[1].add_sprite(
     'swordandshield',
@@ -82,11 +93,17 @@ max_speed = 100000.0
 shield_angle = 0
 max_shield_delta = math.tau / 6
 
+time = 0
+
+enemies = []
 
 @event
 def update(dt, keyboard):
+    global time
     global movement
     global shield_angle
+
+    time += dt
 
     if keyboard.escape:
         sys.exit("quittin' time!")
@@ -115,8 +132,8 @@ def update(dt, keyboard):
     if movement.magnitude() > max_speed:
         movement.scale_to_length(max_speed)
 
-    circle.pos += movement * dt
-    sword_and_shield.pos = circle.pos
+    player.pos += movement * dt
+    sword_and_shield.pos = player.shape.pos = player.pos
 
     def update_shield(source, vector):
         dist, degrees = vector.as_polar()
@@ -177,6 +194,126 @@ def update(dt, keyboard):
         if direction:
             update_shield("buttons", direction)
 
+    for enemy in enemies:
+        enemy.update(dt)
+
+
+random_vector_offset = Vector2(screen_center.y / 2, 0)
+
+class BadGuy:
+    def __init__(self):
+        self.pos = Vector2()
+
+    min_random_distance = 150
+    min_random_distance_squared = min_random_distance ** 2
+    def random_placement(self):
+        # displacement = random_vector_offset.rotate(random.random() * 360)
+        # self.shape.pos = self.pos = screen_center + displacement
+        while True:
+            pos = Vector2(random.randint(0, scene.width), random.randint(0, scene.height))
+            delta = player.pos - pos
+            distance_squared = delta.magnitude_squared()
+            if distance_squared > self.min_random_distance_squared:
+                self.pos = pos
+                print("enemy random pos", self.pos)
+                break
+
+    speed = 1
+
+    def move_towards_player(self):
+        delta = player.pos - self.pos
+        if delta.magnitude() <= self.speed:
+            self.shape.pos = player.pos
+            player_collided(self)
+            self.remove()
+            return
+        delta.scale_to_length(self.speed)
+        self.pos += delta
+        self.shape.pos = self.pos
+
+    def remove(self):
+        enemies.remove(self)
+        self.shape.delete()
+
+
+class Stalker(BadGuy):
+    speed = 2
+
+    def __init__(self):
+        super().__init__()
+        self.shape = scene.layers[0].add_star(
+            outer_radius=10,
+            inner_radius=4,
+            points = 6,
+            color=(255, 128, 0),
+            )
+        self.random_placement()
+
+    def update(self, dt):
+        self.move_towards_player()
+
+class Shot(BadGuy):
+    speed = 5
+    lifetime = 2
+
+    def __init__(self, shooter):
+        super().__init__()
+        self.shooter = shooter
+        self.pos = Vector2(shooter.shape.pos[0], shooter.shape.pos[1])
+        self.layer = scene.layers[-1]
+        self.shape = self.layer.add_circle(
+            radius=2,
+            pos=self.pos,
+            color="white",
+        )
+        self.after = time + self.lifetime
+
+    def update(self, dt):
+        if time > self.lifetime:
+            self.remove()
+            return
+        self.move_towards_player()
+
+
+class Shooter(BadGuy):
+    min_time = 0.5
+    max_time = 1.5
+
+    speed = 0.5
+
+    def __init__(self):
+        super().__init__()
+        self.shape = scene.layers[0].add_rect(
+            width=10,
+            height=10,
+            fill=True,
+            color=(128, 0, 255),
+            )
+        self.random_placement()
+
+        self._next_shot_time()
+
+    def _next_shot_time(self):
+        self.next_shot_time = time + self.min_time + (random.random() * (self.max_time - self.min_time))
+
+    def shoot(self):
+        # print("shoot!", self)
+        shot = Shot(self)
+        enemies.append(shot)
+        self._next_shot_time()
+
+    def update(self, dt):
+        self.move_towards_player()
+        if time >= self.next_shot_time:
+            self.shoot()
+
+
+
+for i in range(15):
+    enemies.append(Stalker())
+
+for i in range(8):
+    enemies.append(Shooter())
 
 
 run()  # keep this at the end of the file
