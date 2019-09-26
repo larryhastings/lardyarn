04f8e68f045a1769a10b3df44afaa7b10862818d
Daniel Pope <mauve@mauveweb.co.uk>
2019-09-26 10:59:25 +0100

    Correctly resolve corner cases

--- triangle_intersect.py
+++ triangle_intersect.py
@@ -1,30 +1,57 @@
 #!/usr/bin/env python3
-from wasabi2d import Vector2
+import numpy as np
 
 
-def polygon_collision(poly, circle):
+def polygon_collision(poly, circle_pos, circle_radius):
     edges = zip(poly, poly[1:] + poly[:1])
+    pen_depth = np.inf
+    pen = None
+    depths = []
     for i, (a, b) in enumerate(edges):
         along_x, along_y = (b - a).normalize()
         across = Vector2(-along_y, along_x)
         off = across.dot(a)
 
-        if across.dot(circle.pos) < off - circle.radius:
-            return False
-    return True
+        depth = across.dot(circle_pos) - off + circle_radius
+        depths.append(depth)
+        if depth < 0:
+            return None
+        if depth < pen_depth:
+            pen_depth = depth
+            pen = across * depth
+    points = np.array(poly)
+
+    relpts = points - circle_pos
+    dists = np.hypot(relpts[:, 0], relpts[:, 1])
+
+    closest = np.argmin(dists, axis=0)
+    close_point = relpts[closest]
+
+    if dists[closest] < 1e-5:
+        return pen
+
+    to_corner = close_point / np.linalg.norm(close_point)
+    dists = np.dot(relpts, to_corner)
+    if np.all(dists >= closest - 1e-5):
+        if dists[closest] > circle_radius:
+            return None
+        else:
+            return Vector2(*to_corner * (circle_radius - dists[closest]))
+
+    return pen
 
 
 if __name__ == "__main__":
-
-    from wasabi2d import Scene, event, run, keys, Vector2
-    from pygame import joystick
+    from wasabi2d import Scene, event, run, Vector2
 
     scene = Scene()
+    scene.background = (0.2,) * 3
+    scene.layers[0].set_effect('dropshadow', radius=1)
 
     triangle = [
-        Vector2(100, 400),
-        Vector2(300, 500),
-        Vector2(200, 600)
+        Vector2(100, 200),
+        Vector2(300, 300),
+        Vector2(200, 400)
     ]
 
     scene.layers[0].add_polygon(triangle, fill=False, color='yellow')
@@ -33,6 +60,10 @@ if __name__ == "__main__":
     @event
     def on_mouse_move(pos):
         circ.pos = pos
-        circ.color = 'red' if polygon_collision(triangle, circ) else 'green'
+        pen = polygon_collision(triangle, pos, circ.radius)
+
+        if pen:
+            circ.pos -= pen
+        circ.color = 'red' if pen else 'green'
 
     run()
